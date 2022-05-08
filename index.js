e2dee9f3e4c62bc34b551d81d9e01ef888535df2
Mishad5 <abdullahahmedkhan100@gmail.com>
2022-05-08 19:22:44 +0600

    heroku setup'

--- index.js
+++ index.js
@@ -82,6 +82,15 @@ async function run() {
             res.send(items)
         })
 
+        //for jwt
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({ accessToken });
+        })
+
 
     }
     finally {
